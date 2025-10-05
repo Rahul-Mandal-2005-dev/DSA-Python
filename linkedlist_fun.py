12ad60e2dab82d56fb7b04624322a20a7200bc7a
Rahul Mandal <rahulmandal74100@gmail.com>
2025-10-05 21:23:05 +0530

    pushed

--- linkedlist_fun.py
+++ linkedlist_fun.py
@@ -1,31 +1,31 @@
 class Node:
-    def __init__(self, val):
+    def __init__(self, val: int) -> None:
         self.data = val
         self.next = None
 
 
 class LinkedList:
-    def __init__(self):
+    def __init__(self) -> None:
         self.head = None
         self.tail = None
 
-    def insertHead(self, val):
-        newNode = Node(val)
+    def insertHead(self, val: int) -> int:
+        newNode: Node = Node(val)
         if (self.head == None):
             self.head = self.tail = newNode
         else:
             newNode.next = self.head
             self.head = newNode
 
-    def insertTail(self, val):
-        newNode = Node(val)
+    def insertTail(self, val: int) -> int:
+        newNode: Node = Node(val)
         if (self.head == None):
             self.head = self.tail = newNode
         else:
             self.tail.next = newNode
             self.tail = newNode
 
-    def insertIndex(self, val, idx):
+    def insertIndex(self, val: int, idx: int) -> None:
         newNode = Node(val)
         pos = 2
         if (self.head == None):
@@ -34,34 +34,43 @@ class LinkedList:
             newNode.next = self.head
             self.head = newNode
         else:
-            temp = self.head
+            temp: Node = self.head
             while (self.head.next and pos != idx):
                 temp = temp.next
                 pos += 1
             newNode.next = temp.next
             temp.next = newNode
 
-    def deleteHead(self):
-        temp = self.head
+    def deleteHead(self) -> None:
+        temp: Node = self.head
         temp = temp.next
         del self.head
         self.head = temp
 
-    def delateTail(self):
-        temp = self.head
+    def delateTail(self) -> None:
+        temp: Node = self.head
         while (temp.next and temp.next != self.tail):
             temp = temp.next
         del self.tail
         self.tail = temp
 
-    # def deleteIndex(self, idx):
-    #     temp = self.head
-    #     pos = 2
-    #     while (temp.next and pos != idx):
+    def deleteIndex(self, idx: int) -> None:
+        temp: Node = self.head
+        pos: int = 2
+        if (idx == -1):
+            self.head = temp.next
+            del temp
+        else:
+            while (temp.next and pos != idx):
+                temp: Node = temp.next
+                pos += 1
+            delNode = temp.next
+            temp.next = temp.next.next
+            del delNode
 
 
-    def Display(self):
-        temp = self.head
+    def Display(self) -> None:
+        temp: Node = self.head
         while (temp):
             print(temp.data, end="--->")
             temp = temp.next
@@ -72,7 +81,6 @@ l1 = LinkedList()
 
 l1.insertTail(10)
 l1.insertTail(20)
-l1.insertHead(30)
+l1.insertTail(30)
 l1.insertTail(40)
-
 
